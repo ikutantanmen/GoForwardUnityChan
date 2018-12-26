46a6b6e012363629ce3724dd2c6a9f96b0afb110
ikutashunya <s.ikuta1008@gmail.com>
2018-12-26 16:02:01 +0900

    GoForwardUnityChanプロジェクト

---
+++ Assets/HitPlaySound.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HitPlaySound : MonoBehaviour
+{
+
+    private AudioSource sound01;
+
+    void Start()
+    {
+        //AudioSourceコンポーネントを取得し、変数に格納
+        sound01 = GetComponent<AudioSource>();
+    }
+
+    void Update()
+    {
+
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        string yourTag = collision.gameObject.tag;
+
+        if (yourTag == "Cube" || yourTag == "Ground")
+        {
+            sound01.PlayOneShot(sound01.clip);
+        }
+    }
+}
