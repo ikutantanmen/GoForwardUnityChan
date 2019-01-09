fc10738beabc99a253e675282858819c22203f27
ikutashunya <s.ikuta1008@gmail.com>
2019-01-09 18:05:59 +0900

    ブロックが壊れた際の効果音を追加

--- Assets/CubeController.cs
+++ Assets/CubeController.cs
@@ -3,6 +3,7 @@ using System.Collections;
 
 public class CubeController : MonoBehaviour
 {
+    [SerializeField] GameObject m_explosionPrefab;
 
     // キューブの移動速度
     private float speed = -0.2f;
@@ -10,10 +11,17 @@ public class CubeController : MonoBehaviour
     // 消滅位置
     private float deadLine = -10;
 
+    // 爆発効果音
+    public AudioClip explosionSE;
+
+    private AudioSource audioSource;
+
     // Use this for initialization
     void Start()
     {
-
+        audioSource = gameObject.AddComponent<AudioSource>();
+        audioSource.clip = explosionSE;        // 鳴らす音(変数)を格納.
+        audioSource.loop = false;
     }
 
     // Update is called once per frame
@@ -34,8 +42,11 @@ public class CubeController : MonoBehaviour
         // もしもtagがbulletであるオブジェクトと接触したら
         if (collision.gameObject.tag == "Bullet")
         {
+            // オーディオを再生
+            AudioSource.PlayClipAtPoint(explosionSE, transform.position);
+            //爆発のエフェクト追加
+            Instantiate(m_explosionPrefab, this.transform.position, Quaternion.identity);
             Destroy(gameObject);
         }
     }
 }
-
